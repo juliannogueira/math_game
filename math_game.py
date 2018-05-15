45ace1b84f7eaba62662f46a3470756cddedaf9d
julian torres <juliannogueira@yahoo.com>
2018-05-15 10:58:23 -1000

    The first math problem was created.

--- math_game.py
+++ math_game.py
@@ -7,7 +7,7 @@ while True:
     answer_one = input()
 
     if answer_one == 'YES' or answer_one == 'Yes' or answer_one == 'yes':
-        print('Awesome! I will give you a fun math quiz!\nIt will consist of (3) questions')
+        print('\nAwesome! I will give you a fun math quiz!\nIt will consist of (3) questions')
         break
 
     elif answer_one == 'NO' or answer_one == 'No' or answer_one == 'no':
@@ -17,3 +17,26 @@ while True:
 
     else:
         print('I did not quite understand that! Forgive me! Please enter \'yes\' or \'no\'.')
+
+
+
+
+#The next block of code will hold the first math question.
+
+print('You will only be given (1) chance to get each question correct. So, just answer correctly.\n')
+
+print('Here is the first question: ', end = '')
+print('What is 2 + 2? Please enter a number.')
+answer_two = input()
+
+if answer_two == '4':
+    print('You got it!')
+
+else:
+    print('\nThat is incorrect.')
+    print('Program is being terminated in...')
+    for counter in range(1, 7):
+        print(6 - counter)
+    print('Goodbye.')
+    exit()
+    
