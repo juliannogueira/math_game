361fb3be3af49dd8dc185c79420bf788966043fe
julian torres <juliannogueira@yahoo.com>
2018-05-15 11:34:44 -1000

    Code was edited, and a function was made to terminate the program if a wrong answer is entered.

--- math_game.py
+++ math_game.py
@@ -1,13 +1,26 @@
+#This function will be used to terminate the program if a user answers a question incorrectly.
+
+def terminator():
+    print('\nThat is incorrect.')
+    print('The program will be terminated in:')
+    for counter in range(1,7):
+        print('...' + str(6 - counter))
+    print('Goodbye.')
+    exit()
+
+
+
+
 #This initial block of code simply asks the user if she or he likes math. The answer will dictate whether to continue the program
 #or not.
 
-while True:
+print('Hello! Do you like math?')
 
-    print('Hello! Do you like math?')
+while True:
     answer_one = input()
 
     if answer_one == 'YES' or answer_one == 'Yes' or answer_one == 'yes':
-        print('\nAwesome! I will give you a fun math quiz!\nIt will consist of (3) questions')
+        print('\nAwesome! I will give you a fun math quiz!\nIt consists of (3) questions.')
         break
 
     elif answer_one == 'NO' or answer_one == 'No' or answer_one == 'no':
@@ -23,9 +36,9 @@ while True:
 
 #The next block of code will hold the first math question.
 
-print('You will only be given (1) chance to get each question correct. So, just answer correctly.\n')
+print('You are only given (1) chance to answer each question correctly. So, just get it right.')
 
-print('Here is the first question: ', end = '')
+print('\nHere is the first question: ', end = '')
 print('What is 2 + 2? Please enter a number.')
 answer_two = input()
 
@@ -33,10 +46,6 @@ if answer_two == '4':
     print('You got it!')
 
 else:
-    print('\nThat is incorrect.')
-    print('Program is being terminated in...')
-    for counter in range(1, 7):
-        print(6 - counter)
-    print('Goodbye.')
-    exit()
+    terminator()
+
     
