93226389d110b762847862322da7688d6e083297
julian torres <juliannogueira@yahoo.com>
2018-05-18 03:36:35 -1000

    All of the math problems were converted into functions. This will allow for easy navigation in changing the questions.

--- math_game.py
+++ math_game.py
@@ -1,9 +1,68 @@
-#These variables will hold the math questions.
-question_one = 'What is 2 + 2 ? Please enter a number.'
+#This function simply asks the user if she or he likes math, and per the response; the program will continue running, or it will exit.
 
-question_two = 'What is ((4*2) + 8)/4 ? Please enter a number.'
+def starting_point():
 
-question_three = 'How do you spell \'4\' ? Please use capital letters.'
+    while True:
+        like_math = input()
+
+        if like_math == 'YES' or like_math == 'Yes' or like_math == 'yes':
+            print('\nAwesome! I will give you a fun math quiz!\nIt consists of (3) questions.')
+            break
+
+        elif like_math == 'NO' or like_math == 'No' or like_math == 'no':
+            print('Okay! Have a nice day! Enter anything to quit.')
+            input()
+            exit()
+
+        else:
+            print('I did not quite understand that! Forgive me! Please enter \'yes\' or \'no\'.') 
+
+
+
+
+#Here are the functions for each question.
+
+def question_one():
+
+    print('What is 2 + 2 ? Please enter a number.')
+    answer_one = input()
+
+    if answer_one == '4':
+        print('You got it! Amazing!')
+
+    else:
+        terminator()
+
+
+def question_two():
+
+    print('What is ((4*2) + 8)/4 ? Please enter a number.')
+    answer_two = input()
+
+    if answer_two == '4':
+        print('It is true! You do like math!')
+    else:
+        terminator()
+
+
+def question_three():
+
+    print('How do you spell \'4\' ? Please use capital letters.')
+    answer_three = input()
+
+    if answer_three == 'FOUR':
+        print('Wow! You did fantastic!')
+        print('\nEnter anything to quit.')
+        input()
+        print('Goodbye.')
+        exit()
+
+    elif answer_three == 'Four' or answer_three == 'four':
+        print('You were so close, but so far...')
+        terminator()
+
+    else:
+        terminator()
 
 
 
@@ -25,21 +84,7 @@ def terminator():
 #or not.
 
 print('Hello! Do you like math?')
-
-while True:
-    like_math = input()
-
-    if like_math == 'YES' or like_math == 'Yes' or like_math == 'yes':
-        print('\nAwesome! I will give you a fun math quiz!\nIt consists of (3) questions.')
-        break
-
-    elif like_math == 'NO' or like_math == 'No' or like_math == 'no':
-        print('Okay! Have a nice day! Enter anything to quit.')
-        input()
-        exit()
-
-    else:
-        print('I did not quite understand that! Forgive me! Please enter \'yes\' or \'no\'.')
+starting_point()
 
 
 
@@ -47,16 +92,8 @@ while True:
 #The next block of code will hold the first math question.
 
 print('You are only given (1) chance to answer each question correctly. So, just get it right.')
-
 print('\nHere is the first question: ', end = '')
-print(question_one)
-answer_one = input()
-
-if answer_one == '4':
-    print('You got it! Amazing!')
-
-else:
-    terminator()
+question_one()
 
 
 
@@ -64,13 +101,7 @@ else:
 #This block of code holds the second math question.
 
 print('\nHere is the next question: ', end = '')
-print(question_two)
-answer_two = input()
-
-if answer_two == '4':
-    print('It is true! You do like math!')
-else:
-    terminator()
+question_two()
 
 
 
@@ -78,19 +109,4 @@ else:
 #Here is the third and final question.
 
 print('\nHere is the final question: ', end = '')
-print(question_three)
-answer_three = input()
-
-if answer_three == 'FOUR':
-    print('Wow! You did fantastic!')
-    print('\nEnter anything to quit.')
-    input()
-    print('Goodbye.')
-    exit()
-
-elif answer_three == 'Four' or answer_three == 'four':
-    print('You were so close, but so far...')
-    terminator()
-
-else:
-    terminator()
+question_three()
